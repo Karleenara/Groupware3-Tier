ee2cada57591afdefb04b6040fb9a05ddb82a929
nara6488 <nara6488@naver.com>
2023-04-28 17:14:15 +0900

    230428_pt

--- src/main/java/com/ds/gw/pt/controller/AdminController.java
+++ src/main/java/com/ds/gw/pt/controller/AdminController.java
@@ -3,6 +3,8 @@ package com.ds.gw.pt.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,59 +16,40 @@ import com.ds.gw.bt.dto.DeptDto;
 import com.ds.gw.bt.dto.HUDto;
 import com.ds.gw.bt.dto.HobbyDto;
 import com.ds.gw.bt.dto.UserDto;
+import com.ds.gw.pt.service.ApiService;
 
 
 
 @Controller
 public class AdminController {
-	
+	@Resource(name="apiService")
+	ApiService service;
 	private final RestTemplate restTemplete = new RestTemplate();
 	
 	@RequestMapping(value="/admin")
-	public String getUser(Model model) {
-		List<UserDto> userlist = restTemplete.getForObject("http://localhost:9090/api/userlist", List.class);
-		String searchKeyword = restTemplete.getForObject("http://localhost:9090/api/searchKeyword", String.class);
+	public String getUser(UserDto u_dto, Model model) {
+		List<UserDto> userlist = service.getUser(u_dto);
 		model.addAttribute("userlist", userlist);
-		model.addAttribute("searchKeyword", searchKeyword);
 		return "admin";
 	}
 	
 	// 상세정보 보기
 	@RequestMapping("/admin/{user_id}")
-	public String getView(@PathVariable String user_id, UserDto u_dto, HUDto hu_dto, Model model ) {
-		List<UserDto> userlist = restTemplete.getForObject("http://localhost:9090/api/userlist", List.class);
-		List<HobbyDto> hobbylist = restTemplete.getForObject("http://localhost:9090/api/hobbylist", List.class);
-		List<DeptDto> deptlist = restTemplete.getForObject("http://localhost:9090/api/deptlist", List.class);
-//		String searchKeyword = restTemplete.getForObject("http://localhost:9090/api/searchKeyword", String.class);
-		
+	public String getView(@PathVariable String user_id, UserDto u_dto,HobbyDto h_dto, DeptDto d_dto, HUDto hu_dto, Model model ) {
+		List<UserDto> userlist = service.getUser(u_dto);
 		model.addAttribute("getList", userlist);
+		
+		List<DeptDto> deptlist = service.getDept(d_dto);
 		model.addAttribute("deptlist", deptlist);
 		
+		List<HobbyDto> hobbylist = service.getHobby(h_dto);
+		model.addAttribute("hobbylist", hobbylist);
 		
-		URI uri = UriComponentsBuilder
-				.fromUriString("http://localhost:9090")
-				.path("/api/getview")
-				.queryParam("user_id", user_id)
-				.encode()
-				.build()
-				.toUri();
-		
-		URI uri2 = UriComponentsBuilder
-				.fromUriString("http://localhost:9090")
-				.path("/api/userhobby")
-				.queryParam("user_id", user_id)
-				.encode()
-				.build()
-				.toUri();
-		
-		String list_eh = restTemplete.getForObject(uri2, String.class);
+		String list_eh = service.viewuh(user_id, hu_dto);
 		model.addAttribute("userHobby", list_eh);
 		
-		UserDto resultdto = restTemplete.getForObject(uri, UserDto.class);
-		model.addAttribute("hobbylist", hobbylist);
+		UserDto resultdto = service.getView(user_id, u_dto);
 		model.addAttribute("userDto", resultdto);
-		
-		
 		
 		return "view";
 		
@@ -75,17 +58,17 @@ public class AdminController {
 	// update 정보 저장
 	@RequestMapping(value="/admin/save/{user_id}")
 	public String updateUser(@PathVariable String user_id,UserDto u_dto,HUDto hu_dto, Model model ) {
-		List<UserDto> userlist = restTemplete.getForObject("http://localhost:9090/api/userlist", List.class);
+		List<UserDto> userlist = service.getUser(u_dto);
 		model.addAttribute("getList", userlist);
 		
-		restTemplete.postForObject("http://localhost:9090/api/updateUser", u_dto, UserDto.class);
+		service.updateUser(user_id, u_dto);
 		
-		restTemplete.postForObject("http://localhost:9090/api/deleteHobby", hu_dto, HUDto.class);
+		service.deleteHobby(user_id, hu_dto);
 		String hulist = hu_dto.getHobby_cd();
 		String[] hulist2 = hulist.split(",");
 		for (int i=0; i<hulist2.length; i++) {
 			hu_dto.setHobby_cd(hulist2[i]);
-			restTemplete.postForObject("http://localhost:9090/api/insertHobby", hu_dto, HUDto.class);
+			service.insertHobby(user_id, hu_dto);
 		}
 		
 		return "redirect:/admin";
@@ -94,11 +77,11 @@ public class AdminController {
 	
 	@RequestMapping("/admin/delete/{user_id}")
 	public String deleteUser(@PathVariable String user_id,UserDto u_dto,HUDto hu_dto, Model model ) {
-		List<UserDto> userlist = restTemplete.getForObject("http://localhost:9090/api/userlist", List.class);
+		List<UserDto> userlist = service.getUser(u_dto);
 		model.addAttribute("getList", userlist);
 		
-		restTemplete.postForObject("http://localhost:9090/api/deleteUser", u_dto, UserDto.class);
-		restTemplete.postForObject("http://localhost:9090/api/deleteHobby", hu_dto, HUDto.class);
+		service.deleteUser(user_id, u_dto);
+		service.deleteHobby(user_id, hu_dto);
 		return "redirect:/admin";
 		
 	}
---
+++ src/main/java/com/ds/gw/pt/service/ApiService.java
@@ -0,0 +1,94 @@
+package com.ds.gw.pt.service;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.ds.gw.bt.dto.DeptDto;
+import com.ds.gw.bt.dto.HUDto;
+import com.ds.gw.bt.dto.HobbyDto;
+import com.ds.gw.bt.dto.UserDto;
+
+@Service
+public class ApiService {
+	private final RestTemplate restTemplete = new RestTemplate();
+	
+// ------------user----------------
+	public List<UserDto> getUser(UserDto u_dto){
+		String searchKeyword = u_dto.getSearchKeyword();
+		String uri = "http://localhost:9090/api/userlist?searchKeyword="+searchKeyword;
+		List<UserDto> userlist = restTemplete.getForObject(uri, List.class);
+		return userlist;
+	}
+	
+	public UserDto getView(String user_id,UserDto u_dto) {
+		URI uri = UriComponentsBuilder
+				.fromUriString("http://localhost:9090")
+				.path("/api/getview")
+				.queryParam("user_id", user_id)
+				.encode()
+				.build()
+				.toUri();
+		UserDto resultdto = restTemplete.getForObject(uri, UserDto.class);
+		return resultdto;
+	}
+	
+	public void insertUser(UserDto u_dto) {
+		String uri = "http://localhost:9090/api/insertUser";
+		restTemplete.postForObject(uri, u_dto, UserDto.class);
+	}
+	
+	public void updateUser(String user_id, UserDto u_dto) {
+		String uri = "http://localhost:9090/api/updateUser";
+		restTemplete.postForObject(uri, u_dto, UserDto.class);
+	}
+	
+	public void deleteUser(String user_id, UserDto u_dto) {
+		String uri = "http://localhost:9090/api/deleteUser";
+		restTemplete.postForObject(uri, u_dto, UserDto.class);
+	}
+	
+// -----------dept-----------------
+	public List<DeptDto> getDept(DeptDto d_dto){
+		String uri = "http://localhost:9090/api/deptlist";
+		List<DeptDto> deptlist = restTemplete.getForObject(uri, List.class);
+		return deptlist;
+	}
+	
+	
+// -----------hobby------------------
+	public List<HobbyDto> getHobby(HobbyDto h_dto){
+		String uri = "http://localhost:9090/api/hobbylist";
+		List<HobbyDto> hobbylist = restTemplete.getForObject(uri, List.class);
+		return hobbylist;
+	}
+	
+// -----------userhobby-------------
+	public String viewuh(String user_id,HUDto hu_dto) {
+		URI uri = UriComponentsBuilder
+				.fromUriString("http://localhost:9090")
+				.path("/api/userhobby")
+				.queryParam("user_id", user_id)
+				.encode()
+				.build()
+				.toUri();
+		String list_eh = restTemplete.getForObject(uri, String.class);
+		return list_eh;
+		
+	}
+	
+	public void insertHobby(String user_id, HUDto hu_dto) {
+		String uri = "http://localhost:9090/api/insertHobby";
+		restTemplete.postForObject(uri, hu_dto, HUDto.class);
+	}
+	
+	public void deleteHobby(String user_id, HUDto hu_dto) {
+		String uri = "http://localhost:9090/api/deleteHobby";
+		restTemplete.postForObject(uri, hu_dto, HUDto.class);
+	}
+	
+
+}
